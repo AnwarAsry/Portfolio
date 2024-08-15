91c29d816ea22387fd51214edbc881f6a5fb529d
AnwarAsry <anwareducation03@gmail.com>
2024-08-15 21:23:27 +0200

    Done with media on hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,10 +5,7 @@ export const Hero = () => {
         <section className="hero">
             <div className="intro">
                 <div className="intro__text">
-                    <h2 className="intro__text--greeting">Hi there, my name is
-                        <br />
-                        Anwar Asry
-                    </h2>
+                    <h2 className="intro__text--greeting">Hi there, my name is Anwar Asry</h2>
                     <p className="intro__text--sentence">I am a <span className="occupation">Frontend-developer</span> based in Stockholm, Sweden.</p>
                 </div>
                 <img src={profile} alt="Image of Developer" className="intro__pic"/>
