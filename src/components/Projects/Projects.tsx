9250d3a013a30f9c02a06baeea2b65f850552a81
AnwarAsry <anwareducation03@gmail.com>
2024-08-16 21:34:11 +0200

    Added new past projects section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -11,6 +11,14 @@ export const Projects = () => {
                 <ProjectPresentation />
                 <ProjectPresentation />
             </div>
+            <h2 className="projects__title">Past projects</h2>
+            <hr />
+            <div className="projects__wrapper">
+                <ProjectPresentation />
+                <ProjectPresentation />
+                <ProjectPresentation />
+                <ProjectPresentation />
+            </div>
         </section>
     </>
 }
