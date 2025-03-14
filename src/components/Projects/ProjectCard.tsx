219d3b351616c0853f2047a7f5d147fdc3ea7e0f
AnwarAsry <anwareducation03@gmail.com>
2025-03-14 19:59:53 +0100

    Added live indecation

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -11,10 +11,17 @@ export const ProjectCard = ({ project }: IProjectCardProps) => {
     return <>
         <div className={`grid ${project.image ? "grid-cols-single md:grid-cols-2" : "grid-cols-single"} gap-8 rounded-2xl shadow-sm bg-[#313445] overflow-hidden`}>
             <div className={`py-8 px-5 md:py-17 ${project.image ? "md:pl-14 md:pr-0" : "md:px-14"}`}>
-                <h3 className="mb-3">
-                    <a className="font-semibold text-3xl text-white" href={project.repository} target="_blank">
-                        {project.name}
+                {project.liveDemo && (
+                    <a className="flex gap-2 items-center text-green-400 hover:underline" href={project.liveDemo} target="_blank">
+                        <span className="relative flex items-center justify-center size-3">
+                            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-300 opacity-75"></span>
+                            <span className="relative inline-flex size-2 rounded-full bg-green-400"></span>
+                        </span>
+                        Live Demo
                     </a>
+                )}
+                <h3 className="mt-1 mb-3 font-semibold text-3xl text-white">
+                    {project.name}
                 </h3>
                 {
                     project.topics && <TagsPresentation topics={project.topics} />
