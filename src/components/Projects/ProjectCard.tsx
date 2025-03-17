0d0515a137a29346445cfd3400ec4f52afe4bb4d
AnwarAsry <anwareducation03@gmail.com>
2025-03-17 19:04:39 +0100

    DONE

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -2,6 +2,7 @@ import { Buttons } from "@components/Buttons";
 import { Project } from "@models/Project";
 import { TagsPresentation } from "@components/Tags/TagsPresentation";
 import { FaArrowRight } from "react-icons/fa6";
+import { RiSpam2Fill } from "react-icons/ri";
 
 interface IProjectCardProps {
     project: Project;
@@ -9,10 +10,10 @@ interface IProjectCardProps {
 
 export const ProjectCard = ({ project }: IProjectCardProps) => {
     return <>
-        <div className={`relative grid ${project.image ? "grid-cols-[grid-template-columns:minmax(18rem,67rem)] md:grid-cols-2" : "grid-cols-[grid-template-columns:minmax(18rem,67rem)]"} gap-8 rounded-2xl shadow-sm bg-[#313445] overflow-hidden`}>
-            {project.personal && <div className="px-3 absolute right-0 rounded-bl-lg text-white bg-sky-600">
+        <div className={`relative grid ${project.image ? "grid-cols-[grid-template-columns:minmax(18rem,67rem)] md:grid-cols-2" : "grid-cols-[grid-template-columns:minmax(18rem,67rem)]"} gap-8 rounded-2xl shadow-md bg-[#313445] overflow-hidden`}>
+            {project.personal && <span className="px-4 py-0.5 absolute right-0 rounded-bl-lg text-sm sm:text-base text-white bg-sky-600">
                 Personal
-            </div>}
+            </span>}
             <div className={`p-5 md:py-8 ${project.image ? "md:pl-8 md:pr-0" : "md:px-8"}`}>
                 {project.liveDemo && (
                     <a className="flex gap-2 items-center text-green-400 hover:underline" href={project.liveDemo} target="_blank">
@@ -23,13 +24,14 @@ export const ProjectCard = ({ project }: IProjectCardProps) => {
                         Live Demo
                     </a>
                 )}
-                <h3 className="mt-1 mb-3 font-semibold text-3xl text-white">
+                <h3 className="mt-1 mb-0.5 font-bold text-xl sm:text-3xl text-white">
                     {project.name}
                 </h3>
+                <span className="mb-5 block text-base font-normal  text-white">{project.created} - {project.lastUpdated}</span>
                 {
                     project.topics && <TagsPresentation topics={project.topics} />
                 }
-                <p className="text-lg text-[#d0d5dd]">
+                <p className="text-base sm:text-lg text-[#d0d5dd]">
                     {project.description ? project.description : "No description"}
                 </p>
                 <Buttons style="mt-8" type="secondary" link={project.repository}>
@@ -37,7 +39,7 @@ export const ProjectCard = ({ project }: IProjectCardProps) => {
                     <FaArrowRight />
                 </Buttons>
             </div>
-            {project.image && <img className="hidden md:block" src={project.image} alt="Image of project" loading="lazy" />}
+            {project.image && <img className="hidden md:block" src={project.image} alt={project.name} loading="lazy" />}
         </div>
     </>
 }
